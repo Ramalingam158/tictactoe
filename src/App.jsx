d6282dd4002394cde900650fea0ffb844f59b82f
Ramalingam <ramalingamveerabahu@gmail.com>
2022-07-22 23:36:03 +0530

    Implemented the winnning logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
 
 import './styles/root.scss';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const msg = winner
+    ? `The winner is ${winner}`
+    : `Next move: ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h3>{msg}</h3>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
